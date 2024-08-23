20f1f564da8cbf5fa7897c3b052d79dc4a9c2b88
Ward Greunke <wgreunke@gmail.com>
2024-08-23 11:19:37 -0700

    Update test_tidb_connection.py

--- test_tidb_connection.py
+++ test_tidb_connection.py
@@ -1,10 +1,2 @@
-#This tests the connection to tidby
-#App is availible here: https://test-tidb.streamlit.app/
-import streamlit as st
-
-
-
-
-
-
-st.write("If you can see this, the script is running fine")
+#Do not use this file.  I have created a second github for testing here:  https://github.com/wgreunke/tidb-test/tree/main.
+#There was a problem with the requirements.txt file in this repository and streamlit.
