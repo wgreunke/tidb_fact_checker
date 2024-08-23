dfef43edc841629f245efa12f98685495f4838f4
Ward Greunke <wgreunke@gmail.com>
2024-08-22 22:30:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
 import streamlit as st
 st.write("Hello world")
-name=st.input("Please enter your name")
-st.write(name)
+statement=st.text_input("Please enter a fact to check:")
+
+st.write(statement)
+
+
+#Chunk fact
+
+#Send fact to tidb
+
+#Show search results
+
+#Send to llm
+
+#Show whether true or false.
+#Show source of document / add link to source
+
