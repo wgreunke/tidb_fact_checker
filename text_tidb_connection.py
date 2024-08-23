52a71c11709319feccf7e7e79aa014d079882236
Ward Greunke <wgreunke@gmail.com>
2024-08-23 10:54:20 -0700

    Update text_tidb_connection.py

--- text_tidb_connection.py
+++ text_tidb_connection.py
@@ -1,1 +1,10 @@
 #This tests the connection to tidby
+#App is availible here: https://test-tidb.streamlit.app/
+import streamlit
+
+
+
+
+
+
+st.write("If you can see this, the script is running fine")
