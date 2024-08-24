d6e021054abb716dc881162a24a032514af50506
Ward Greunke <wgreunke@gmail.com>
2024-08-23 19:28:08 -0700

    Delete test_tidb_connection.py

--- test_tidb_connection.py
+++
@@ -1,2 +0,0 @@
-#Do not use this file.  I have created a second github for testing here:  https://github.com/wgreunke/tidb-test/tree/main.
-#There was a problem with the requirements.txt file in this repository and streamlit.
